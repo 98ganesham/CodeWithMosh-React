f07fa8cc6b1df78cdec0d074ade2a8f27a76309e
98Ganesham <98ganesham@gmail.com>
2023-06-21 16:50:47 +0630

    React

--- React/vite/react-app/src/App.tsx
+++ React/vite/react-app/src/App.tsx
@@ -1,41 +1,11 @@
-// import { useState } from "react";
-// import ListGroup from "./2.Styling Components/ListGroup";
-// import Buttons from "./1.Building Components/Components/Buttons";
-// import Alert from "./1.Building Components/Exercise/Alert";
-// import { BsFillCalendarFill } from "react-icons/bs";
-// import Alert from "./1.Building Components/Components/Alert";
-// import Buttons from "./1.Building Components/Components/Buttons";
-import Like from "./2.Styling Components/Building A Like Component/Like";
+// import ExpandableText from "./3.Managing Component State/Exercises/ExpandableText";
+import Form from "./4.Building Forms/Form";
 
 function App() {
-  // const [alertVisible, setAlertVisibility] = useState(false);
-  // // const items = ["New York", "Los Angeles", "San Francisco"];
-
   return (
     <div>
-      <Like onClick={() => console.log("clicked")} />;
+      <Form />
     </div>
   );
 }
 export default App;
-
-// <div>
-//   <BsFillCalendarFill color="lightgreen" />
-// </div>
-// app tsx for ListGroup
-// <div>
-//   <ListGroup heading="Miami" items={items} onSelectItem={() => {}} />
-// </div>
-// app tsx for MyAlert
-// <div>
-//   {alertVisible && (
-//     <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-//   )}
-
-//   <Buttons color="info" onClick={() => setAlertVisibility(true)}>
-//     My Button
-//   </Buttons>
-//     // </div>
-//   );
-// }
-// export default App;
--- React/vite/react-app/src/main.tsx
+++ React/vite/react-app/src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./4.Building Forms/index.css";
 import App from "./App";
-// import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
